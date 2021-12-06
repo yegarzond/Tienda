669ef6028f78d0e056a21e1c9e5496265f85279e
lmigueldeavila <lmigueldeavila@gmail.com>
2021-12-05 22:35:23 -0500

    serializers4.0

--- Backend/appTienda/serializers/facturaSerializer.py
+++ Backend/appTienda/serializers/facturaSerializer.py
@@ -12,54 +12,32 @@ from appTienda.models.user import User
 
 class FacturaSerializer(serializer.ModelSerializer):
 
-    user = UserSerializer()
+
     itemsFactura = ItemsFacturaSerializer()
-    producto = ProductoSerializer()
+
 
     class Meta:
         model: factura
-        fields: ['id','fecha','tipo_doc','num_doc', 'nombres','apellidos','total_items','total_factura', 'user','itemsFactura', 'producto']
+        fields: ['id','fecha','tipo_doc','num_doc', 'nombres','apellidos','total_items','total_factura','itemsFactura']
 
-    def create(self, validated_data):
-        user_data = validated_data.pop('user')
-        facturaInstance = Factura.objects.create(**validated_data)
-        User.objects.create(factura = facturaInstance, **user_data)
-        return facturaInstance
-    
     def create(self, validated_data):
         itemsFactura_data = validated_data.pop('itemsFactura')
         facturaInstance = Factura.objects.create(**validated_data)
-        ItemsFactura.objects.create(factura = facturaInstance, **itemsFactura_data)
+        itemsFactura.objects.create(factura = facturaInstance, **itemsFactura_data)
         return facturaInstance
-    
+         
     def to_representation(self, obj):
         factura = Factura.objects.get(id = obj.id)
-        user = User.objects.get(factura = obj.id)
-        itemsFactura = ItemsFactura.objects.get(factura = obj.id)
+        itemsFactura = ItemsFactura.objects.get(itemsFactura = obj.id)
         return {
             
-            'fecha': factura.fecha,
-
-            'user': {
-                'name': user.name,
-                'email': user.mail,
-            },
-
-            'producto': {
-                'ref': producto.ref,
-                'nombre': producto.nombre,
-                'marca': producto.marca,
-                'unidad_medida': producto.unidad_medida,
-                'precio': producto.precio
-            },
-
+            'fecha': factura.fecha, 
+            'total_items': factura.total_items,
+            'total_factura': factura.total_factura,
             'itemsFactura':{
                 'unidades': itemsFactura.unidades,
                 'precio': itemsFactura.precio,
                 'subtotal': itemsFactura.subtotal
-            },
-
-            'total_items': factura.total_items,
-            'total_factura': factura.total_factura
+            }
         }
 
