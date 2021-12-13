920e3cf6d771f1c1f7aba3197ddd41c1b0a80c77
Adriana Franklin Sanchez <adriana.franklin@sofka.com.co>
2021-12-13 07:53:02 -0500

    trabajo con template de productos sin exito

--- Front/app-tienda/src/components/Productos.vue
+++ Front/app-tienda/src/components/Productos.vue
@@ -1,48 +1,135 @@
 <template>
-  <div class="cointainer-menu" >
-    <h3>producto categoria </h3>
-    <table class="centered container-table" >
-      <thead >
-        <tr >
-          <th>nombre producto</th>
-          <th>descripcion</th>
-          <th>precio</th>
+  <div class="cointainer-menu">
+    <h3>Listado de productos</h3>
+    <button @click="modalCrear = true">Crear Producto</button>
+
+    <div v-if="modalCrear" class="centrar-modal">
+      <transition name="fade">
+        <div class="row">
+          <div class="col s12">
+            <div class="card">
+              <div class="card-content">
+                <span class="card-title">Crear Producto</span>
+                <form class="col s12">
+                  <div class="row">
+                    <div class="input-field col s6">
+                      
+                  </div>
+                  </div>
+                </form>
+              </div>
+              <div class="card-action bton">
+                <button @click="crearProducto()">Guardar</button>
+                <button>Cancelar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </div>
+
+    <table class="centered container-table">
+      <thead>
+        <tr>
+          <th>Nombre Producto</th>
+          <th>Categoria</th>
+          <th>Marca</th>
+          <th>Precio</th>
+          <th>Unidades Disponibles</th>
         </tr>
       </thead>
       <tbody>
-        <tr >
-          <td>morfeo me llama</td>
-          <td>morfeo me llama1</td>
-          <td>morfeo me llama2</td>
-
+        <tr v-for="item in productos" :key="item.nombre">
+          <td>{{ item.nombre }}</td>
+          <td>{{ item.categoria }}</td>
+          <td>{{ item.marca }}</td>
+          <td>{{ numberFormat(item.precio) }}</td>
+          <td>{{ item.unidades_disponibles }}</td>
+          <td>
+            <button
+              class="teal accent-4"
+              dark
+              @click="listarProducto(producto.id)"
+            >
+              Editar
+            </button>
+            <button class="error" darck @click="borrarProducto(producto.id)">
+              Eliminar
+            </button>
+          </td>
         </tr>
       </tbody>
-      <!-- <li>nombre producto {{}} descripcion {{}} precio</li> -->
     </table>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import NProgress from "nprogress";
+
 export default {
   name: "Productos",
   data: function () {
     return {
-      productos:[]
+      productos: [],
+      producto: {},
+      nuevoProducto: {
+        categoria: "",
+        marca: "",
+        nombre: "",
+        precio: 0,
+        ref: "",
+        unidad_medida: "",
+        unidades_disponibles: 0,
+      },
+      modalCrear: false,
     };
   },
+  computed: {},
+  created: function () {
+    this.listarProductos();
+  },
+
   methods: {
-    producto() {
+    numberFormat(value) {
+      const formatter = new Intl.NumberFormat("es-CO", {
+        style: "currency",
+        currency: "COP",
+        minimumFractionDigits: 0,
+      });
+
+      return formatter.format(value);
+    },
+    // showModalCrear() {
+    //   this.modalCrear == true;
+    //   console.log("Modal")
+    // },
+
+    listarProductos() {
       NProgress.start();
       axios
         .get("http://127.0.0.1:8000/producto/")
         .then((response) => {
-          console.log(response);
-          console.log(this.productos)
+          this.productos = [...response.data];
+          console.log(this.productos);
         })
         .catch((error) => {
           console.log(error);
         })
         .then(() => NProgress.done());
+    },
+    crearProducto() {
+      axios
+        .post("http://127.0.0.1:8000/producto/crear/")
+        .then((response) => {
+          // this.nuevoProducto.categoria = response.data.categoria;
+          console.log(response.data);
+          this.listarProductos();
+        })
+
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
 };
@@ -72,5 +159,9 @@ export default {
 .container-table {
   margin: 0 0 12px 0px;
   background-color: rgb(255 255 255 / 70%);
+}
+
+.centrar-modal {
+  z-index: 999999999;
 }
 </style>
--- Front/app-tienda/src/main.js
+++ Front/app-tienda/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import 'nprogress/nprogress.css'
 import { createPinia } from 'pinia'
 
+
 createApp(App).
 use(router).
 use(createPinia()).
